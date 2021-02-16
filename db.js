66dcbb6c6fb1f4bfdd857b2357ab5c28d58743c8
Laura <laurasarabiaf@gmail.com>
2021-02-16 11:39:13 -0300

    agrega mensajes

---
+++ db.js
@@ -0,0 +1,61 @@
+const Sequelize = require('sequelize');
+
+// acá creamos la conexión a la Base de Datos
+const sql = new Sequelize('tablero', 'root', 'laurasarabia', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+// acá inicializamos los modelos (tablas)
+const Message = sql.define('Message', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    
+  },
+  message: {
+    type: Sequelize.STRING,
+    allowNull: false,    
+  }
+});
+
+const Comment = sql.define('Comment', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    
+  },
+  comment: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+});
+
+//User.hasMany(Message);  // User "tiene muchas" Message
+//Message.belongsTo(User); // Cada "Message" pertenece a un "User"
+Message.hasMany(Comment);
+Comment.belongsTo(Message);
+//Comment.belongsTo(User); //cada comentario pertenece a un usuario
+
+//  después sincronizamos nuestro código con la base de datos
+sql.sync()
+.then(() => {
+  console.log('Base de datos y tablas creadas');
+});  
+
+// finalmente acá listamos todos los modelos que queremos exportar
+module.exports = {
+  //User,
+  Message,
+  Comment
+};
---
+++ routes.js
@@ -0,0 +1,50 @@
+const { name } = require('ejs');
+const { Router } = require('express');
+const { Message, Comment } = require('./db');
+const router = Router();
+
+router.get('/', async (req, res) => {  
+  const messages = await Message.findAll({
+    include: [Comment]
+  });
+
+  const comments = await Comment.findAll({
+    include: [Message]
+  });
+
+  console.log(messages);
+  res.render('index.ejs', {messages: messages, comments: comments})
+});
+
+router.post("/message",  async (req, res) => { 
+	let newMessage = await Message.create({
+    name: req.body.name,
+    message: req.body.message
+  });		
+	res.redirect('/');
+});
+
+
+
+router.post("/comment",  async (req, res) => { 
+  let newComment = await Comment.create({
+    name: req.body.cname,
+    comment: req.body.comment,
+    messageId: req.body.messageId
+  });   
+  console.log(newComment);
+  res.redirect('/');
+});
+
+
+//yaaaaa
+
+//router.post("/comment/:message._id",  async (req, res) => { 
+
+
+
+
+
+
+
+module.exports = router;
